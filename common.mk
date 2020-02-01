61a414c9f2c9ed2c4b870519ddea4b8103772b49
SagarMakhar <sagarmakhar@gmail.com>
2020-02-02 01:42:56 +0530

    sm6125-common: Don't enforce RRO on all targets
    Signed-off-by: SagarMakhar <sagarmakhar@gmail.com>

--- common.mk
+++ common.mk
@@ -30,9 +30,7 @@ DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay-lineage \
     $(LOCAL_PATH)/overlay-system
 
-PRODUCT_ENFORCE_RRO_TARGETS := *
 PRODUCT_ENFORCE_RRO_EXCLUDED_OVERLAYS += \
-    $(LOCAL_PATH)/overlay-lineage/lineage-sdk \
     $(LOCAL_PATH)/overlay-lineage/packages/apps/Snap \
     $(LOCAL_PATH)/overlay-system
 
