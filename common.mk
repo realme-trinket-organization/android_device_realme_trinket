1617aaff20441bdde7d1ce2dfd98cb8deb63c610
sbwml <984419930@qq.com>
2020-12-10 13:49:03 +0800

    sm6125-common: bump vendor.display.config to 2.0
    * New WFD blobs need vendor.display.config@2.0.so

--- common.mk
+++ common.mk
@@ -206,8 +206,7 @@ PRODUCT_PACKAGES += \
     android.hardware.renderscript@1.0-impl \
     android.frameworks.displayservice@1.0 \
     vendor.qti.hardware.display.allocator-service \
-    vendor.display.config@1.1 \
-    vendor.display.config@1.1_vendor
+    vendor.display.config@2.0
 
 # Display interfaces
 PRODUCT_PACKAGES += \
