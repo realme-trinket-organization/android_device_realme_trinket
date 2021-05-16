b243dd03d9fb3c5ee34c57de272847281cea8931
DarkJoker360 <simoespo159@gmail.com>
2021-05-16 16:03:02 +0200

    sm6125-common: Update graphics mapper

--- common.mk
+++ common.mk
@@ -201,7 +201,8 @@ PRODUCT_PACKAGES += \
     android.frameworks.displayservice@1.0_32 \
     android.hardware.graphics.allocator@2.0-impl \
     android.hardware.graphics.allocator@2.0-service \
-    android.hardware.graphics.mapper@2.0-impl-qti-display \
+    android.hardware.graphics.mapper@3.0-impl-qti-display \
+    android.hardware.graphics.mapper@4.0-impl-qti-display \
     android.hardware.graphics.composer@2.3-service \
     android.hardware.memtrack@1.0-impl \
     android.hardware.memtrack@1.0-service \
