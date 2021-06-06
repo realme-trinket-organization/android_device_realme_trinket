2d5456c5b864275d7e18ab417c04f3ff84d6513f
Alin Jerpelea <alin.jerpelea@sony.com>
2021-06-06 16:59:57 +0700

    sm6125-common: remove deprecated display packages
    Those packages were used by legacy devices and can be removed
    
    Signed-off-by: Alin Jerpelea <alin.jerpelea@sony.com>
    Signed-off-by: Nantaaa <anantah738@gmail.com>

--- common.mk
+++ common.mk
@@ -188,9 +188,6 @@ PRODUCT_PACKAGES += \
     libqdMetaData.system
 
 PRODUCT_PACKAGES += \
-    android.frameworks.displayservice@1.0_32 \
-    android.hardware.graphics.allocator@2.0-impl \
-    android.hardware.graphics.allocator@2.0-service \
     android.hardware.graphics.mapper@3.0-impl-qti-display \
     android.hardware.graphics.mapper@4.0-impl-qti-display \
     android.hardware.graphics.composer@2.4-impl \
