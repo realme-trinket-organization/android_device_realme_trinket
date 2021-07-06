266d746383ca2436e152e32713d1b524f0d67956
dodyirawan85 <40514988+dodyirawan85@users.noreply.github.com>
2021-07-06 07:31:59 +0000

    realme_trinket: Bring back device init identifier
    * Refs
    https://github.com/dev-harsh1998/android_device_realme_X2/blob/lineage-18.1/init/x2.cpp
    https://github.com/Thagoo/android_device_xiaomi_rova/blame/lineage-18.1/init/init_rova.cpp
    
    Signed-off-by: dodyirawan85 <40514988+dodyirawan85@users.noreply.github.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -116,6 +116,10 @@ DEVICE_MATRIX_FILE := $(DEVICE_PATH)/configs/manifests/compatibility_matrix.xml
 ODM_MANIFEST_19632_FILES := $(DEVICE_PATH)/configs/manifests/manifest_19632.xml
 ODM_MANIFEST_SKUS += 19632
 
+# Init
+TARGET_INIT_VENDOR_LIB := //$(DEVICE_PATH):libinit_realme_trinket
+TARGET_RECOVERY_DEVICE_MODULES := libinit_realme_trinket
+
 # Keystore
 TARGET_PROVIDES_KEYMASTER := true
 
