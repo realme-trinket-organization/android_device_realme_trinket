ff0f32f890f8de63a2031c3ae6d4565612036311
nantaaa <anantah738@gmail.com>
2021-08-05 08:49:38 +0000

    realme_trinket: Import powerhint from laurel sprout.

--- device.mk
+++ device.mk
@@ -377,6 +377,9 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
      android.hardware.power@1.3-service.realme_trinket-libperfmgr
 
+PRODUCT_COPY_FILES += \
+     $(LOCAL_PATH)/configs/powerhint.json:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.json
+
 # Protobuf
 PRODUCT_PACKAGES += \
     libprotobuf-cpp-full-vendorcompat \
